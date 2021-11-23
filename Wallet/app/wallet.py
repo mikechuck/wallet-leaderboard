67d8aa0408f8b7f8d38224a895eb3bddcf8d0012
Michael Joy <mcjoy465@gmail.com>
2021-11-22 22:43:03 -0500

    updated db design and routes logic

--- Wallet/app/wallet.py
+++ Wallet/app/wallet.py
@@ -1,7 +1,4 @@
-# goals
-# create a new wallet
-# add item to existing wallet
-# retrieve an item from an existing wallet
+
 # this should work for different versions of the mobile game client (playing v1 and v2 of the game)
 
 import json
@@ -11,186 +8,140 @@ from bson.objectid import ObjectId
 from flask import Flask, request, jsonify
 
 client = MongoClient("mongodb://localhost:27017")
-# database is called "wallets"
-# will have tables: PlayerWallets and WalletItems
 db = client.Wallets
-playersDB = db["Players"]
+playersCollection = db["Players"]
+walletsCollection = db["Wallets"]
 
 app = Flask(__name__)
 
-def getUserFromAuth(header):
-	# retrieving user information from auth
-	return {
-		"player_id": 123456
-	}
+def getUserFromAuth(headers):
+	if (headers.get("Authorization")):
+		# Retrieving user information from provider
+		#  ...
+		return {
+			"player_id": 123456
+		}
+	else:
+		return {}
 
-# {
-#    "_id" : ObjectId("5c6d73090c3d5054b766a76e"),
-#    "EmployeeName" : "Larry",
-#    "EmployeeSalary" : 9000,
-#    "EmployeeDetails" : [
-#       {
-#          "EmployeeDOB" : ISODate("1990-01-21T00:00:00Z"),
-#          "EmployeeDepartment" : "ComputerScience",
-#          "EmployeeProject" : [
-#             {
-#                "Technology" : "C",
-#                "Duration" : 6
-#             },
-#             {
-#                "Technology" : "Java",
-#                "Duration" : 7
-#             }
-#          ]
-#       }
-#    ]
-# }
-
-# {
-# 	"_id": "5c6d73090c3d5054b766a76e",
-# 	"PlayerName": "Test",
-# 	"Wallets": [
-# 		{
-# 			"WalletName": "test",
-# 			"Items": [
-# 				{
-# 					"ItemName": "testItem",
-# 					""
-# 				}
-# 			]
-# 		}
-# 	]
-# }
-
-@app.route('/wallet', methods=['POST'])
+@app.route('/wallets', methods=['POST'])
 def createWallet():
-	authHeader = request.headers.get("Authorization")
-	userInfo = getUserFromAuth(authHeader)
-	eventBody = json.loads(request.data)
-	# Input body params
-	walletName = eventBody.get("wallet_name")
+	# Validate authorization
+	playerId = getUserFromAuth(request.headers).get("player_id")
+	if (not playerId):
+		return {"Message": "Missing authorization"}, 403
 
-	# Create id for new wallet
+	# Input body params
+	eventBody = json.loads(request.data)
+	walletName = eventBody.get("wallet_name")
 	walletId = str(uuid.uuid4())
 
+	# Validate required input fields
 	if not walletName:
 		return {"Message": "Missing fields: wallet_name"}, 400
 
-	playersDB.update(
-		{ 
-			"_id": userInfo.get("player_id")
-		}, 
-		{
-			"$push": {
-				"Wallets": {
-					"_id": walletId,
-					"WalletName": walletName, 
-					"Items": [] 
-				}
+	try:
+		# Add id to Players.Wallets
+		playersCollection.update_one(
+			{ "_id": playerId }, 
+			{ "$push": {"Wallets": walletId} }
+		);
+
+		# Create new Wallet document in Wallets collection (with empty Items array)
+		walletsCollection.insert_one(
+			{
+				"_id": walletId,
+				"PlayerId": playerId,
+				"WalletName": "New Wallet",
+				"Items": []
 			}
-		}
-	);
+		)
 
-	return {"Message": "Success"}, 200
+		return walletId, 200
+	except:
+		return {"Message": "Internal Error"}, 500
 
 
-@app.route('/wallet/item', methods=['POST'])
+
+@app.route('/wallets/items', methods=['POST'])
 def addItemToWallet():
-	authHeader = request.headers.get("Authorization")
-	userInfo = getUserFromAuth(authHeader)
+	# Validate authorizations
+	playerId = getUserFromAuth(request.headers).get("player_id")
+	if (not playerId):
+		return {"Message": "Missing authorization"}, 403
 
-	eventBody = json.loads(request.data)
 	# Input body params
+	eventBody = json.loads(request.data)
 	walletId = eventBody.get("wallet_id")
 	itemName = eventBody.get("item_name")
 	itemPrice = eventBody.get("item_price") or ""
 	itemColor = eventBody.get("item_color") or ""
 
+	# Validate required input fields
 	if not walletId or not itemName:
 		return {"Message": "Missing fields: wallet_id or item_name"}, 400
 
 	try:
-		# playersDB.update(
-		# 	{ 
-		# 		"_id": userInfo.get("account_id"),
-		# 		"Wallets._id": walletId
-		# 	}, 
-		# 	{
-		# 		"$push": {
-		# 			"Wallets.$.Items": {
-		# 				"ItemName": itemName, 
-		# 				"ItemPrice": itemPrice,
-		# 				"ItemColor": itemColor  
-		# 			}
-		# 		}
-		# 	}
-		# );
+		# Create id for new item
+		itemId = str(uuid.uuid4())
 
-		# updateResponse = playersDB.update(
-		# 	{ 
-		# 		"_id": userInfo.get("account_id"), 
-		# 		"Wallets._id": walletId
-		# 	},
-		# 	{ "$push": 
-		# 		{"Wallets.$[].Items": 
-		# 			{
-		# 				"ItemName": itemName,
-		# 				"ItemPrice": itemPrice,
-		# 				"ItemColor": itemColor
-		# 			}
-		# 		}
-		# 	}
-		# )
-
-		returnItem = playersDB.find(
+		# Add new item to Wallet.Items array
+		walletsCollection.update_one(
+			{ 
+				"_id": walletId,
+				"PlayerId": playerId
+			}, 
 			{
-				"Wallets._id": walletId
+				"$push": {
+					"Items": {
+						"_id": itemId,
+						"ItemName": itemName, 
+						"ItemPrice": itemPrice,
+						"ItemColor": itemColor  
+					}
+				}
 			}
-		)
+		);
 
-
-		print("returnItem:", returnItem)
-
-		return {"Message": "Success"}, 200
+		return itemId, 200
 	except:
-		return {"Message": "Error"}, 500
+		return {"Message": "Internal Error"}, 500
 
-@app.route('/wallet/<walletId>/item/<itemId>', methods=['GET'])
+
+@app.route('/wallets/<walletId>/items/<itemId>', methods=['GET'])
 def getItemFromWallet(walletId, itemId):
-	authHeader = request.headers.get("Authorization")
-	userInfo = getUserFromAuth(authHeader)
+	# Validate authorization
+	playerId = getUserFromAuth(request.headers).get("player_id")
+	if (not playerId):
+		return {"Message": "Missing authorization"}, 403
 
-	print("walletId:", walletId)
-	print("itemId:", itemId)
-
+	# Validate input fields
 	if not walletId or not itemId:
 		return {"Message": "Missing fields: wallet_id or item_id"}, 400
+	
+	try:
+		# Query for the itemId, and filter by matching on only that item
+		cur = walletsCollection.find(
+			{
+				"PlayerId": playerId,
+				"Items._id": itemId
+			}, 
+			{
+				"Items": {
+					"$elemMatch": {
+						"_id": itemId
+					}
+				}
+			}
+		);
 
+		# Get item from returned document
+		returnItem = {}
+		for doc in cur:
+			returnItem = doc.get("Items")[0]
 
-	cur = playersDB.find(
-		{
-			"_id": userInfo.get("player_id"),
-			"Wallets._id": walletId,
-			"Wallets.Items._id": itemId
-		}
-	)
-	# cur = playersDB.find(
-	# 	{
-	# 		"Wallets._id": walletId
-	# 	}
-	# )
-	returnItem = {}
-	for doc in cur:
-		print("doc:", doc)
-		returnItem = doc.get("Wallets")[0].get("Items")[0]
-	# if (len(cur)):
-	# 	returnItem = cur[0]
-	# else:
-	# 	return {"Message": "Item not found", "Item": ""}, 201
-
-	# query = {"wallet_id": walletId, "item_id": itemId}
-	# returnItem = walletItemsDB.find_one(query)
-
-	return {"Message": "Success", "Item": returnItem}, 200
+		return returnItem, 200
+	except:
+		return {"Message": "Internal Error"}, 500
 
 app.run(debug=True)
